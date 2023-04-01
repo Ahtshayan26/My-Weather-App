9313db93db2cae27136cd035d2ad92d7339fb9af
Ahtshayan26 <udayasanthiran.2019359@iit.ac.lk>
2023-04-01 11:03:50 +0530

    Initial Commit

---
+++ My Weather App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  My Weather App
+//
+//  Created by Niranjan on 2023-04-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My Weather App/My_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Weather_AppApp.swift
+//  My Weather App
+//
+//  Created by Niranjan on 2023-04-01.
+//
+
+import SwiftUI
+
+@main
+struct My_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
